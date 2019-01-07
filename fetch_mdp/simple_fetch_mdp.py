c4b4033bdcfd3c6643d22240a56fda6d27cfabad
Yuchuan <yyc478@gmail.com>
2019-01-08 00:06:15 +0800

    simplify observation format

--- fetch_mdp/simple_fetch_mdp.py
+++ fetch_mdp/simple_fetch_mdp.py
@@ -21,14 +21,13 @@ class SimpleFetchMdp(GoalEnv):
         self._arm_location = self._location_space.sample()
         self._picked_up_block = False
 
-        self.action_displacements =  [
-            (lambda s: s - 1),  # right
-            (lambda s: s - self.x_dim),  # up
-            (lambda s: s + 1),  # left
-            (lambda s: s + self.x_dim),  # down
+        self.action_handlers =  [
+            self._move_function(lambda s: s - 1, lambda s: s % self.x_dim == 0),  # right
+            self._move_function(lambda s: s - self.x_dim, lambda s: s < self.x_dim),  # up
+            self._move_function(lambda s: s + 1, lambda s: (s + 1) % self.x_dim == 0),  # left
+            self._move_function(lambda s: s + self.x_dim, lambda s: s + self.x_dim >= self.x_dim * self.y_dim),  # down
+            self._grab
         ]
-        self.action_handlers = [self._move_function(displace) for displace in self.action_displacements]
-        self.action_handlers.append(self._grab)
 
     def reset(self):
         # Pick a random goal and block location
@@ -57,6 +56,25 @@ class SimpleFetchMdp(GoalEnv):
         info = []
         return obs, reward, done, info
 
+    # Shortcut for setting the state and getting the output of the action
+    def step_for(self, state, action, obs_format='dict'):
+        self._goal_location = state[0]
+        self._arm_location = state[2]
+        if state[1] == -1:
+            self._block_location = self._arm_location
+            self._picked_up_block = True
+        else:
+            self._block_location = state[1]
+            self._picked_up_block = False
+
+        result = self.step(action)
+        if obs_format == 'dict':
+            return result
+        return ([
+            result[0]['desired_goal'],
+            result[0]['achieved_goal'],
+            result[0]['observation']
+        ], ) + result[1:]
 
     def compute_reward(self):
         if self._arm_location == self._goal_location and self._picked_up_block:
@@ -66,8 +84,8 @@ class SimpleFetchMdp(GoalEnv):
     def _get_obs(self):
          return dict(
             desired_goal=self._goal_location,
-            achieved_goal=self._block_location,
-            observation=[self._arm_location, self._picked_up_block]
+            achieved_goal= -1 if self._picked_up_block else self._block_location,
+            observation=self._arm_location
         )
 
     def _grab(self):
@@ -75,8 +93,10 @@ class SimpleFetchMdp(GoalEnv):
             self._picked_up_block = True
 
 
-    def _move_function(self, displace):
+    def _move_function(self, displace, no_move_if):
         def ret():
+            if no_move_if(self._arm_location):
+                return
             self._arm_location=displace(self._arm_location)
             if self._picked_up_block:
                 self._block_location = displace(self._block_location)
