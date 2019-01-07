260b69087f14e3cb6caba303bdf441457174f5c5
Yuchuan <yyc478@gmail.com>
2019-01-08 00:06:37 +0800

    generate new trajectories and add example

--- generate_simple_trajectories.py
+++ generate_simple_trajectories.py
@@ -2,14 +2,12 @@ import gym
 from gym.envs.registration import register
 import numpy as np
 
-
 register(
     id='FetchMDP-v0',
     entry_point='fetch_mdp:SimpleFetchMdp',
 )
 
 env = gym.make('FetchMDP-v0')
-
 
 GRIDWORLD_X = 5
 GRIDWORLD_Y = 5
@@ -21,12 +19,12 @@ action_displacements = [
     (lambda s: s + GRIDWORLD_X),  # down
 ]
 
+
 def record(traj, observation, action):
-    stateaction=[
+    stateaction = [
         observation['desired_goal'],
         observation['achieved_goal'],
-        observation['observation'][0],
-        observation['observation'][1],
+        observation['observation'],
         action]
     traj.append(stateaction)
 
@@ -34,29 +32,41 @@ def record(traj, observation, action):
 def compute_trajectory():
     observation = env.reset()
     traj = []
+
     def move_to_goal(pos, goal):
         while pos != goal:
             nonlocal observation
             # So ties between equally good choices are broken at random
             valid_choices = []
-            x_coords = (pos % GRIDWORLD_X),(goal % GRIDWORLD_X)
+            x_coords = (pos % GRIDWORLD_X), (goal % GRIDWORLD_X)
             if x_coords[0] != x_coords[1]:
                 valid_choices.append(2 if x_coords[0] < x_coords[1] else 0)
-            y_coords = (np.floor(pos / GRIDWORLD_X)),(np.floor(goal/ GRIDWORLD_X))
+            y_coords = (np.floor(pos / GRIDWORLD_X)), (np.floor(goal / GRIDWORLD_X))
             if y_coords[0] != y_coords[1]:
                 valid_choices.append(3 if y_coords[0] < y_coords[1] else 1)
             action = np.random.choice(valid_choices)
 
-
             record(traj, observation, action)
             observation, reward, done, info = env.step(action)
-            pos = observation['observation'][0]
-    move_to_goal(observation['observation'][0], observation['achieved_goal'])
+            pos = observation['observation']
+
+    move_to_goal(observation['observation'], observation['achieved_goal'])
     record(traj, observation, 4)  # Grab
     observation, reward, done, info = env.step(4)
-    move_to_goal(observation['observation'][0], observation['desired_goal'])
+    move_to_goal(observation['observation'], observation['desired_goal'])
     record(traj, observation, None)  # Record final state with no action
     return traj
 
+
 trajectories = [compute_trajectory() for _ in range(1000)]
 np.save('1000_mdp_trajectories', np.array(trajectories))
+
+
+state = (0, 0, 1)
+action = 0
+# You can also get the next state in the environment using:
+observation, reward, done, info = env.step_for(state, action)
+print(observation)
+# Which gives the observation in the openai dict format or
+observation, reward, done, info = env.step_for(state, action, obs_format='tuple')
+print(observation) # goal position, block position, arm position
