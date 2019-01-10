80bd851e4a0ebb13765773ff8394cafdbd2331b1
Yuchuan <yyc478@gmail.com>
2019-01-10 11:03:31 +0800

    fetchreach visualization

--- FetchReach.py
+++ FetchReach.py
@@ -1,5 +1,6 @@
 import gym
 from gym.envs.registration import register
+import numpy as np
 
 register(
     id='FetchMDP-v0',
@@ -12,22 +13,42 @@ env = gym.make('FetchMDP-v0')
 env.reset()
 
 
-GRID_DIMS = [0.25, 0.35]
+GRID_DIMS = [0.25, .35]
 GRID_CENTER = [1.3, 0.75]
-obs = [1.3, 0.75, 0.2]
+info = [1.3, 0.75, 0.2]
 
-for i in range(1000):
+observation, reward, done, info = env.step(4, render=True)
+
+def move_to_goal(goal, observation):
+    for i in range(1000):
+        env.render()
+        actions = env.action_space
+        # actions is a 4-tuple: (forward/backward, right-left, up-down, open-close)
+        current = observation['observation']
+        # TODO: Make the policy stochastic to avoid tiebreaking problems
+        if goal == current:
+            return
+        if goal % 5 > current % 5:
+            action = 2
+        elif goal % 5 < current % 5:
+            action = 0
+        elif np.floor(goal / 5) > np.floor(current / 5):
+            action = 3
+        elif np.floor(goal / 5) < np.floor(current / 5):
+            action = 1
+        print(action)
+        observation, reward, done, info = env.step(action, render=True) # take a random action
+        print(observation)
+
+move_to_goal(observation['achieved_goal'], observation)
+
+action = 4
+observation, reward, done, info = env.step(action, render=True)
+
+move_to_goal(observation['desired_goal'], observation)
+
+action = 4
+observation, reward, done, info = env.step(action, render=True)
+
+while True:
     env.render()
-    actions = env.action_space
-    #action = actions.sample()
-    if obs[1] < (0.75 + 0.175):
-        action = 0
-    elif obs[0] < (1.3 + 0.125):
-        action = 1
-    else:
-        action = 2
-    # actions is a 4-tuple: (forward/backward, right-left, up-down, open-close)
-    print(action)
-    print(obs)
-    observation, reward, done, info = env.step(action, render=True) # take a random action
-    obs = observation['observation']
--- fetch_mdp/fetch_mdp.py
+++ fetch_mdp/fetch_mdp.py
@@ -1,6 +1,6 @@
-from gym.envs.robotics.robot_env import RobotEnv
-from gym.envs.robotics.fetch.pick_and_place import FetchPickAndPlaceEnv
+import os
 from gym.spaces import Discrete, Dict
+from gym.envs.robotics import fetch_env
 
 import numpy as np
 
@@ -8,13 +8,6 @@ import numpy as np
 Wraps the FetchPickAndPlace gym environment in an MDP, so there are only 5 possible actions
 and x * y * 2 possible states 
 """
-
-actions = [
-    (0.,1.,0.,0.),
-    (1.,0.,0.,0.),
-    (0.,-1.,0.,0.),
-    (-1.,0.,0.,0.)
-]
 
 # From https://github.com/openai/gym/blob/master/gym/envs/robotics/fetch_env.py#L76
 # actual displacement when the action is 1.
@@ -28,10 +21,28 @@ MOV_MODIFIER = 0.2
 GRID_DIMS = [0.25, 0.35]
 GRID_CENTER = [1.3, 0.75]
 
-class FetchMdp(FetchPickAndPlaceEnv):
+# file_path = os.path.dirname(os.path.realpath(__file__))
+# MODEL_XML_PATH = os.path.join(file_path, 'fetch_mdp.xml')
+MODEL_XML_PATH = os.path.join('fetch', 'pick_and_place.xml')
 
+class FetchMdp(fetch_env.FetchEnv):
     def __init__(self, x_dim=5, y_dim=5, **kwargs):
-        super(FetchMdp, self).__init__('sparse', **kwargs)
+
+        # From https://github.com/openai/gym/blob/master/gym/envs/robotics/fetch/pick_and_place.py
+        initial_qpos = {
+            'robot0:slide0': 0.405,
+            'robot0:slide1': 0.48,
+            'robot0:slide2': 0.0,
+            'object0:joint': [1.3, 0.75, 0.4, 1., 0., 0., 0.],
+        }
+        fetch_env.FetchEnv.__init__(
+            self, MODEL_XML_PATH, has_object=True, block_gripper=False, n_substeps=20,
+            gripper_extra_height=0.2, target_in_the_air=False, target_offset=0.0,
+            obj_range=0.10, target_range=0.10, distance_threshold=0.05,
+            initial_qpos=initial_qpos, reward_type="sparse")
+
+        self.x_states = x_dim
+        self.y_states = y_dim
         self._old_action_space = self.action_space
         self._old_observation_space = self.observation_space
         self.action_space = Discrete(5) #Right, Up, Left, Down, Grab
@@ -43,35 +54,52 @@ class FetchMdp(FetchPickAndPlaceEnv):
                 target_position=Discrete(x_dim * y_dim + 1)
             ))
         ))
-        self.action_handlers = [self.__move_function(a) for a in actions]
-        self.action_handlers.append(self.__grab)
+        self.action_handlers = [
+            self._move_function((0., 1., 0., -1.), no_move_if=lambda s: s % self.x_states == 0),
+            self._move_function((1., 0., 0., -1.), no_move_if=lambda s: s < self.x_states ),
+            self._move_function((0., -1., 0., -1.), no_move_if=lambda s: (s + 1) % self.x_states == 0),
+            self._move_function((-1., 0., 0., -1.), no_move_if=lambda s: s / self.x_states > (self.y_states - 1))
+]
+        self.action_handlers.append(self._grab)
 
     def step(self, action, render=False):
         response = self.action_handlers[action](render=render)
         obs = response[0]
-        state = self._position_to_state(obs)
+        state = self._process_observation(obs)
         return state, response[1], response[2], response[3]
 
     # the superclass action is a 4-tuple: (forward/backward, right-left, up-down, open-close)
-    def __move_function(self, vector):
-        vector = np.array(vector) * MOV_MODIFIER
+    def _move_function(self, vector, no_move_if=(lambda x: False)):
+        move_vector = np.array(vector) * MOV_MODIFIER
+
         # function is returned and used to handle movement
         def ret(render):
             obs = self._get_obs()
-            current_state = self._position_to_state(obs)
-            for _ in range(2):
+            current_state = self._position_to_state(obs['observation'][:3])
+            # If it's not a valid movement, do nothing
+            if no_move_if(current_state):
+                response = self._step(np.array((0., 0., 0., 0.)))
+                return response
+            while self._position_to_state(obs['observation'][:3]) == current_state:
                 if render:
                     super(FetchMdp, self).render()
-                response = self._step(vector)
+                response = self._step(move_vector)
+                obs = response[0]
             return response
         return ret
 
-    def __grab(self, render):
+    def _grab(self, render):
         open_claw = np.array((0.,0.,0.,1.))
-        for _ in range(2):
+        close_claw = np.array((0., 0., 0., -1.))
+        down = np.array((0., 0., -1., 0.))
+        up = np.array((0., 0., 1., 0.,))
+        movement = [open_claw, down, down, down, close_claw, close_claw, up, up, up]
+        for action in movement:
             if render:
                 super(FetchMdp, self).render()
-            response = self._step(open_claw)
+            response = self._step(action)
+            if(response[1] == 1.):
+                return response
         return response
 
     # Same as in the parent class except removes the np.clip
@@ -81,14 +109,24 @@ class FetchMdp(FetchPickAndPlaceEnv):
         self._step_callback()
         obs = self._get_obs()
 
+        pos = obs['observation']
+
         done = False
         info = {
             'is_success': self._is_success(obs['achieved_goal'], self.goal),
         }
-        reward = self.compute_reward(obs['achieved_goal'], self.goal, info)
-        return obs, reward, done, info
+        reward = self.compute_reward(obs['achieved_goal'], self.goal, info) + 1.
+        return obs, reward, done, pos
 
-    def _position_to_state(self, observation):
-        pos = position['observation'][:3]
-        position['observation'] = pos
-        return position
+    def _position_to_state(self, position):
+        row = np.floor(self.y_states * ((GRID_CENTER[0] + GRID_DIMS[0]/2) - position[0]) / GRID_DIMS[0])
+        col = np.floor(self.x_states * ((GRID_CENTER[1] + GRID_DIMS[1]/2) - position[1]) / GRID_DIMS[1])
+        print((col, row))
+        return row * self.x_states + col
+        return col, row
+
+    def _process_observation(self, obs):
+        obs['achieved_goal'] = self._position_to_state(obs['achieved_goal'])
+        obs['desired_goal'] = self._position_to_state(obs['desired_goal'])
+        obs['observation'] = self._position_to_state(obs['observation'][:3])
+        return obs
